6bf4a72bc90a0c6a49676af58b3ce65e688910e7
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-11-16 16:49:42 +0530

    Create Home.js

---
+++ ClientSide/src/Component/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Topbar from './Topbar';
+import SuperBar from './SuperBar';
+import CarHome from './CarHome';
+import Testimonial from './Testimonial';
+import HomeCard from './HomeCard';
+import Footer from './Footer';
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <SuperBar />
+      <CarHome />
+      <div className="Home">
+        <div id='Testimonial'>
+          <Testimonial />
+          </div>
+        <HomeCard />
+      </div>
+      <Footer />
+    </>
+  );
+}
