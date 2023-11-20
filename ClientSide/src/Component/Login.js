140cb0a797bfde1b32119824432483941a2cd516
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-11-20 14:39:34 +0530

    Update Login.js

--- ClientSide/src/Component/Login.js
+++ ClientSide/src/Component/Login.js
@@ -1,41 +1,74 @@
-import React, { useState } from "react";
+// Login.js
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import Alert from "react-bootstrap/Alert";
+import { useUser } from "./UserContext";
 
 function Login() {
-  const [name, setName] = useState();
-  const [password, setPassword] = useState();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted Data:", { name, password });
   };
-  const navigate = useNavigate();
-  const handleLogin = (e) => {
+
+  const { login } = useUser();
+
+  useEffect(() => {
+    const storedName = localStorage.getItem("name");
+    const storedEmail = localStorage.getItem("email");
+
+    if (storedName && storedEmail) {
+      navigate("/home");
+    }
+  },  [navigate]);
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    axios
-      .post("http://localhost:3001/login", { name, password })
-      .then((response) => {
-        if (response.data && response.data._id) {
-          console.log("Login successful");
-          navigate("/home", { state: { props: response.data } });
-        } else {
-          console.log("Login failed");
-        }
-      })
-      .catch((err) => {
-        console.error("Login error:", err);
+    setLoading(true);
+    setError("");
+    setShowAlert(false);
+
+    try {
+      const response = await axios.post("http://localhost:3001/login", {
+        name,
+        password,
       });
+
+      if (response.data && response.data.token) {
+        console.log("Login successful");
+        login(response.data.name, response.data.email);
+
+        localStorage.setItem("token", response.data.token);
+        navigate("/home");
+      } else {
+        console.log("Login failed");
+        setError(response.data.error || "Invalid credentials");
+        setShowAlert(true);
+      }
+    } catch (err) {
+      console.error("Login error:", err);
+      setError("Invalid Credentials. Please try again.");
+      setShowAlert(true);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <div className="container1">
       <div className="glassmorphism">
         <h1>Welcome to MotorMart</h1>
         <div className="content">
-          <h1>User Login</h1>
-          {/* <button className="btn btn-outline btn-danger" onClick={handleDelete}>
-            Delete Account
-          </button> */}
+          <h3>User Login</h3>
           <form onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name">Name:</label>
@@ -62,22 +95,36 @@ function Login() {
                 }}
               />
             </div>
-            <div className="btn-container" onClick={handleLogin}>
-              <div className="btn">
-                <button type="button">Login</button>
-              </div>
-            </div>
           </form>
+          <div className="btn-container" onClick={handleLogin}>
+            <div className="btn2">
+              <button type="button" disabled={loading}>
+                {loading ? "Logging in..." : "Login"}
+              </button>
+            </div>
+          </div>
           <h3>Don't have an Account? Create One</h3>
           <Link to="/register">
             <div className="btn-container">
-              <div className="btn">
+              <div className="btn2">
                 <button type="submit" onSubmit={handleSubmit}>
                   Register
                 </button>
               </div>
             </div>
           </Link>
+          <div>
+            {showAlert && (
+              <Alert
+                variant="danger"
+                onClose={() => setShowAlert(false)}
+                dismissible
+              >
+                <Alert.Heading>Error!!</Alert.Heading>
+                <p>{error}</p>
+              </Alert>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -85,124 +132,3 @@ function Login() {
 }
 
 export default Login;
-//added modal
-// const handleLogin = (e) => {
-//   e.preventDefault();
-//   axios.post("http://localhost:3001/login", { name, password })
-//     .then((response) => {
-//       if (response.data && response.data._id) {
-//         console.log("Login successful");
-//         navigate('/home', { state: { props: response.data } });
-//       } else {
-//         console.log("Login failed");
-//       }
-//     })
-//     .catch((err) => {
-//       console.error("Login error:", err);
-//     });
-// };
-// const handleDelete = () => {
-//   // Perform the delete operation here
-//   const customerIdToDelete = "654cce24c589b15fb4edd1f7"; // Replace with the actual customer ID
-//   axios.delete(`http://localhost:3001/delete-customer/${customerIdToDelete}`)
-//     .then((response) => {
-//       console.log("Customer deleted successfully");
-//     })
-//     .catch((error) => {
-//       console.error("Error deleting customer:", error);
-//     });
-// };
-// import React, { useState } from "react";
-// import "./Login.css";
-// import { Link, useNavigate} from "react-router-dom";
-// import axios from "axios";
-
-// function Login() {
-//   const [name, setName] = useState();
-//   const [password, setPassword] = useState();
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log("Submitted Data:", { name, password });
-//   };
-//   const navigate = useNavigate();
-//   const handleLogin = (e) => {
-//     e.preventDefault();
-//     axios.post("http://localhost:3001/login", { name, password })
-//       .then((response) => {
-//         if (response.data && response.data._id) {
-//           console.log("Login successful");
-//           navigate('/home', { state: { props: response.data } });
-//         } else {
-//           console.log("Login failed");
-//         }
-//       })
-//       .catch((err) => {
-//         console.error("Login error:", err);
-//       });
-//   };
-
-//   // const deleteit = () =>{
-//   //   axios.delete(`http://localhost:3001/delete-customer/id}`)
-//   //   .then((response) => {
-//   //     console.log("Customer deleted successfully");
-//   //   })
-//   //   .catch((error) => {
-//   //     console.error("Error deleting customer:", error);
-//   //   });
-//   // }
-
-//   return (
-//     <div className="container">
-//       <div className="glassmorphism">
-//         <div className="content">
-//           <h1>User Login</h1>
-//           {/* <button className="btn btn-outline btn-primary">delete</button> */}
-//           <form onSubmit={handleSubmit}>
-//             <div>
-//               <label htmlFor="name">Name:</label>
-//               <input
-//                 type="text"
-//                 placeholder="Enter Name"
-//                 autoComplete="off"
-//                 id="name"
-//                 name="name"
-//                 onChange={(e) => {
-//                   setName(e.target.value);
-//                 }}
-//               />
-//             </div>
-//             <div>
-//               <label htmlFor="password">Password:</label>
-//               <input
-//                 type="password"
-//                 id="password"
-//                 name="password"
-//                 placeholder="Enter Password"
-//                 onChange={(e) => {
-//                   setPassword(e.target.value);
-//                 }}
-//               />
-//             </div>
-//             <div className="btn-container" onClick={handleLogin}>
-//               <div className="btn">
-//                 <button type="button">Login</button>
-//               </div>
-//             </div>
-//           </form>
-//           <h3>Don't have an Account Create One</h3>
-//           <Link to="/register">
-//             <div className="btn-container">
-//               <div className="btn">
-//                 <button type="submit" onSubmit={handleSubmit}>
-//                   Register
-//                 </button>
-//               </div>
-//             </div>
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
