b0c6d7cbb920564b7a8682d6b258f6d745f64079
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-11-20 20:09:41 +0000

    changed profile user page

--- ClientSide/src/Component/UserProfile.js
+++ ClientSide/src/Component/UserProfile.js
@@ -83,7 +83,7 @@ if (!state) {
       <Topbar />
       <SuperBar />
       <div className="user-profile">
-        <img src="https://placekitten.com/200/200" alt="Profile" />
+        <img src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" />
         <h2>{state.name.toUpperCase()}</h2>
         <p>Email: {state.email.toLowerCase()}</p>
       </div>
