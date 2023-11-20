f7fafa0891d232e79f0345d28c4e207ee4fb70fd
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-11-20 14:58:54 +0530

    Update App.js

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -1,13 +1,13 @@
 
 import React from "react";
-import Home from "./Components/Home";
-import Login from "./Components/Login";
-import Services from "./Components/Service";
-import SignUp from "./Components/SignUp";
+import Home from "./Component/Home";
+import Login from "./Component/Login";
+import Services from "./Component/Service";
+import SignUp from "./Component/SignUp";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-import UserProfile from "./Components/UserProfile";
-import UpdateProfileForm from "./Components/UpdateProfileForm";
+import UserProfile from "./Component/UserProfile";
+import UpdateProfileForm from "./Component/UpdateProfileForm";
 
 function App() {
   return (
