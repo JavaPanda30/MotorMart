54b87a072dae1658414f11135bdc16446daf27cc
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-11-21 04:04:42 +0530

    Update UserProfile.js

--- ClientSide/src/Component/UserProfile.js
+++ ClientSide/src/Component/UserProfile.js
@@ -14,8 +14,8 @@ const UserProfile = () => {
   const navigate = useNavigate();
   const [userId, setUserId] = useState(null);
   const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const [bookings, setBookings] = useState([]);
 
-  
   useEffect(() => {
     const getUserData = async () => {
       try {
@@ -24,7 +24,6 @@ const UserProfile = () => {
         );
         const userId = response.data.userId;
         setUserId(userId);
-        console.log("User ID:", userId);
       } catch (error) {
         console.error(
           "Error getting user ID:",
@@ -34,17 +33,36 @@ const UserProfile = () => {
     };
 
     getUserData();
-}, [state.email]);
+  }, [state.email]);
 
-if (!state) {
-  navigate("/login");
-  return null;
-}
+  useEffect(() => {
+    const getBookings = async () => {
+      try {
+        const response = await axios.get(
+          `https://motormart.onrender.com/getBookings/${userId}`
+        );
+        const userBookings = response.data;
+        console.log("booking retreived");
+        setBookings(userBookings);
+      } catch (error) {
+        console.error(
+          "Error getting bookings:",
+          error.response ? error.response.data : error.message
+        );
+      }
+    };
+    getBookings();
+  }, [userId]);
+
+  if (!state) {
+    navigate("/login");
+    return null;
+  }
 
   const handleDelete = async () => {
     try {
       await axios.delete(`https://motormart.onrender.com/delete-customer/${userId}`);
-      console.log("Profile deleted successfully");
+      window.alert("Profile deleted successfully");
       logout();
       navigate("/login");
     } catch (error) {
@@ -61,7 +79,9 @@ if (!state) {
 
   const handleUpdateProfile = async () => {
     try {
-      const emailResponse = await axios.get(`https://motormart.onrender.com/checkEmail/${state.email}`);
+      const emailResponse = await axios.get(
+        `https://motormart.onrender.com/checkEmail/${state.email}`
+      );
       const isEmailExist = emailResponse.data.isExist;
       if (isEmailExist) {
         console.log("Email already exists. Please choose a different email.");
@@ -71,9 +91,28 @@ if (!state) {
       console.log("Profile updated successfully");
       setShowUpdateForm(false);
     } catch (error) {
-      console.error("Error updating profile:", error.response ? error.response.data : error.message);
+      console.error(
+        "Error updating profile:",
+        error.response ? error.response.data : error.message
+      );
     }
   };
+
+  const handleDeleteTestDrive = async (bookingId) => {
+    try {
+      await axios.delete(`https://motormart.onrender.com/delete-booking/${bookingId}`);
+      window.alert("Test drive booking deleted successfully");
+      setBookings((prevBookings) =>
+        prevBookings.filter((booking) => booking._id !== bookingId)
+      );
+    } catch (error) {
+      console.error(
+        "Error deleting test drive booking:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
   const handleCloseUpdateForm = () => {
     setShowUpdateForm(false);
   };
@@ -82,10 +121,18 @@ if (!state) {
     <>
       <Topbar />
       <SuperBar />
+      <div className="imageprofile">
+        <img
+          className="imageprofile"
+          src="https://placekitten.com/200/200"
+          alt="Profile"
+        />
+      </div>
       <div className="user-profile">
-        <img src="https://images.unsplash.com/photo-1494976388531-d1058494cdd8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile" />
         <h2>{state.name.toUpperCase()}</h2>
-        <p>Email: {state.email.toLowerCase()}</p>
+        <center>
+          <h4>Email: {state.email.toLowerCase()}</h4>
+        </center>
       </div>
       <div className="container px-3 py-3 d-flex ">
         <div className="btn btn-danger" onClick={handleDelete}>
@@ -94,6 +141,42 @@ if (!state) {
         <div className="btn btn-primary ms-auto" onClick={handleEdit}>
           Edit Profile
         </div>
+      </div>
+      <div className="container mt-3">
+        <h3>Your Test Drive Bookings</h3>
+        {bookings.length > 0 ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Car Name</th>
+                <th>Timing</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookings.map((booking) => (
+                <tr key={booking._id}>
+                  <td>{booking.name}</td>
+                  <td>{booking.carname}</td>
+                  <td>{booking.timing}</td>
+                  <td>
+                    <button
+                      className="btn btn-outline-danger"
+                      onClick={() => handleDeleteTestDrive(booking._id)}
+                    >
+                      Delete Appointment
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div>
+            <p>No bookings found.</p>
+          </div>
+        )}
       </div>
       {showUpdateForm && userId && (
         <UpdateProfileForm
