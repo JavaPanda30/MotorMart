b7839f61098faeeb00bc2018081889b9334ef048
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-11-20 14:41:02 +0530

    Update SignUp.js

--- ClientSide/src/Component/SignUp.js
+++ ClientSide/src/Component/SignUp.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import "./SignUp.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import Alertmodal from "./Alertmodal";
+import Alert from 'react-bootstrap/Alert';
 
 function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -20,80 +20,86 @@ function SignUp() {
           console.log(result);
           navigate("/login");
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          console.log(err);
+          if (err.response && err.response.status === 401) {
+            setShowAlert(true);
+          }
+        });
     } else {
-      setShowModal(true);
+      setShowAlert(true);
     }
   };
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
   return (
-    <div className="container">
-      
     <div className="container1">
-      <div className="glassmorphism">
-      <h1>Welcome to MotorMart</h1>
-        <div className="content">
-          <h2>User Registration</h2>
-          <form>
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                placeholder="Enter Name"
-                autoComplete="off"
-                id="name"
-                name="name"
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter Email"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter Password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-            </div>
-            <div className="btn-container">
-              <div className="btn">
-                <button type="submit" onClick={handleSubmit}>Submit</button>
+      <div className="container1">
+        <div className="glassmorphism">
+          <h1>Welcome to MotorMart</h1>
+          <div className="content">
+            <h3>User Registration</h3>
+            <form>
+              <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  placeholder="Enter Name"
+                  autoComplete="off"
+                  id="name"
+                  name="name"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
               </div>
-            </div>
-          </form>
-          <h3>Already have an account</h3>
-          <Link to="/login">
-            <div className="btn-container">
-              <div className="btn">
-                <button type="button">Login</button>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter Email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
               </div>
-            </div>
-          </Link>
-    {showModal && (
-      <Alertmodal msg="Fields Can't be Empty" onClose={handleCloseModal} />
-    )}
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="btn-container">
+                <div className="btn2">
+                  <button type="submit" onClick={handleSubmit}>
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+            <h3>Already have an account</h3>
+            <Link to="/login">
+              <div className="btn-container">
+                <div className="btn2">
+                  <button type="button">Login</button>
+                </div>
+              </div>
+            </Link>
+            {showAlert && (
+              <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+                <Alert.Heading>Error!!</Alert.Heading>
+                <p>Fields Can't be Empty</p>
+              </Alert>
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
