85ea146253a9c2cbf53701ba18c3abee2a601682
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-11-20 19:16:27 +0530

    Update Service.js

--- ClientSide/src/Component/Service.js
+++ ClientSide/src/Component/Service.js
@@ -13,7 +13,9 @@ export default function Services() {
             Our <span className="Text">Services</span>
           </h1>
           <p className="servicesp">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Embark on a seamless journey with Motor Mart – where innovation
+            meets elegance, and personalized service elevates your car-buying
+            experience.
           </p>
         </div>
         <div className="ServicesCards1">
@@ -23,9 +25,10 @@ export default function Services() {
                 <i className="  fa-solid fa-headset fa-4x"></i>
                 <p className="h1">Free Support</p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Drive worry-free with Motor Mart! Our commitment goes beyond
+                  cars—enjoy complimentary support, ensuring your ownership
+                  experience is as smooth as your favorite ride. Your
+                  satisfaction, our priority.
                 </p>
               </div>
             </div>
@@ -36,9 +39,9 @@ export default function Services() {
                 <i className="fa-solid fa-truck-fast fa-4x"></i>
                 <p className="h1">Super Fast </p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Experience the thrill of super-fast service at Motor Mart.
+                  From virtual browsing to lightning-quick transactions, we
+                  redefine speed, ensuring you get on the road in record time!
                 </p>
               </div>
             </div>
@@ -49,9 +52,10 @@ export default function Services() {
                 <i className="fa-solid fa-screwdriver-wrench fa-4x"></i>
                 <p className="h1">Repairing</p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Trust our skilled technicians to revive your vehicle to peak
+                  performance. Motor Mart's repairing service: Precision,
+                  expertise, and care for your car's well-being, every step of
+                  the way.
                 </p>
               </div>
             </div>
@@ -65,9 +69,9 @@ export default function Services() {
                 <i className="fa-solid fa-lock fa-4x"></i>
                 <p className="h1">Safety </p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Your safety is our priority. Experience peace of mind with
+                  Motor Mart's comprehensive safety services, ensuring your
+                  journey is not only stylish but secure. Drive with confidence.
                 </p>
               </div>
             </div>
@@ -78,9 +82,9 @@ export default function Services() {
                 <i className="fa-solid fa-fan fa-4x"></i>
                 <p className="h1">Air Conditioning </p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Beat the heat with our top-notch Air Conditioning service. We
+                  keep your car cool and comfortable, ensuring every drive is a
+                  breeze. Trust us to keep you refreshed on the road!
                 </p>
               </div>
             </div>
@@ -91,9 +95,9 @@ export default function Services() {
                 <i className="fa-solid fa-gas-pump fa-4x"></i>
                 <p className="h1">Oil Change</p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Revitalize your ride! Our expert team ensures peak performance
+                  with precision oil changes. Drive with confidence, as we
+                  pamper your engine to keep it purring at its best.
                 </p>
               </div>
             </div>
@@ -107,9 +111,10 @@ export default function Services() {
                 <i className="fa-solid fa-hammer fa-4x"></i>
                 <p className="h1">Engine Repair</p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Revitalize your ride with Motor Mart's expert Engine Repair
+                  service. Precision diagnostics, skilled technicians, and a
+                  commitment to performance restoration. Your engine, our
+                  expertise – unbeatable synergy.
                 </p>
               </div>
             </div>
@@ -120,9 +125,10 @@ export default function Services() {
                 <i className="fa-solid fa-key fa-4x"></i>
                 <p className="h1">Dealership </p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Experience excellence at every turn with our dealership
+                  services. From personalized consultations to top-notch
+                  maintenance, we're dedicated to enhancing your driving
+                  journey. Your satisfaction is our priority.
                 </p>
               </div>
             </div>
@@ -133,9 +139,10 @@ export default function Services() {
                 <i className="fa-regular fa-handshake fa-4x"></i>
                 <p className="h1">Trusted Agents </p>
                 <p className="p1">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                  laboriosam at voluptas minus culpa deserunt delectus sapiente
-                  inventore pariatur
+                  Your journey, our commitment. With Trusted Agents, experience
+                  a car-buying process guided by experts. Trust, transparency,
+                  and excellence define our service. Your satisfaction is our
+                  priority.
                 </p>
               </div>
             </div>
@@ -146,7 +153,7 @@ export default function Services() {
           <div className="Blogimg">
             <img
               className="serviceBlogimg"
-              src="https://images.unsplash.com/photo-1553440569-bcc63803a83d?q=80&w=1725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="https://plus.unsplash.com/premium_photo-1663050653231-d6b2020ea5b0?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
             />
           </div>
@@ -155,31 +162,31 @@ export default function Services() {
               Why Choose Our <span className="Text">Services ?</span>
             </h1>
             <p className="serviceblogp">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-              facere, facilis rem fuga aliquid accusamus, voluptatibus optio
-              quibusdam, ullam quas debitis explicabo illum veniam distinctio
-              minus minima molestiae hic vitae?
+              Choose Motor Mart for an unparalleled car-buying experience. Our
+              virtual showroom blends innovation with elegance, offering
+              personalized test drives and expert advice. Navigate the future
+              with a seamless, secure process tailored to your preferences.
             </p>
             <br />
             <br />
             <p className="serviceblogp">
               <i className="fa-solid fa-check fa-1x"></i>{" "}
-              <span>Super Fast</span>
+              <span>Innovative Experience</span>
             </p>
             <br />
             <p className="serviceblogp">
               <i className="fa-solid fa-check fa-1x"></i>{" "}
-              <span>Trusted Agents</span>
+              <span>Personalized Service</span>
             </p>
             <br />
             <p className="serviceblogp">
               <i className="fa-solid fa-check fa-1x"></i>{" "}
-              <span>Engine Repair</span>
+              <span>Seamless Security</span>
             </p>
             <br />
             <p className="serviceblogp">
               <i className="fa-solid fa-check fa-1x"></i>{" "}
-              <span>Free Support</span>
+              <span>Tailored Excellence</span>
             </p>
             <br />
           </div>
@@ -189,14 +196,15 @@ export default function Services() {
             Schedule <span className="Text">Services</span>
           </h1>
           <p className="serviceformp">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta
-            asperiores sunt quisquam culpa dolorem aliquam dolores explicabo
-            animi voluptatum totam. Reiciendis temporibus iure voluptas minus
-            hic nemo officiis incidunt harum.
+            Transform your car experience with us! Schedule your personalized
+            virtual test drive, expert consultation, and secure transactions
+            today. Elevate your car-buying journey with Motor Mart – where
+            convenience meets excellence. Your dream car awaits, just a click
+            away. Seize the moment and schedule our top-notch services now!
           </p>
           <div className="serviceFormContact">
-            <div className="serviceform">
-              <div className="servicebox">
+            <div className="Contactform">
+              <div className="Formbox">
                 <form>
                   <label for="name"></label>
                   <input
